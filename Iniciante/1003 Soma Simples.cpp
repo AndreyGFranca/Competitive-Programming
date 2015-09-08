c906daed17dfb33ed614ed98cc8dfc1e5d121a5e
AndreygFranca <andrey_franca@hotmail.com.br>
2015-09-08 12:41:09 -0300

    Rename Soma Simples 1003.cpp to 1003 Soma Simples.cpp

--- Iniciante/1003 Soma Simples.cpp
+++ Iniciante/1003 Soma Simples.cpp
@@ -1,4 +1,4 @@
-/* Solução exercicio https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+/* SoluÃ§Ã£o exercicio https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
 Andrey.*/
 
 #include <iostream>
