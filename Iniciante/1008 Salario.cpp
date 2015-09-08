82013454a642f9c9615f722d3ec2539bca6c63f5
AndreygFranca <andrey_franca@hotmail.com.br>
2015-09-08 12:40:36 -0300

    Rename Salario 1008.cpp to 1008 Salario.cpp

--- Iniciante/1008 Salario.cpp
+++ Iniciante/1008 Salario.cpp
@@ -1,4 +1,4 @@
-/* Solução exercicio https://www.urionlinejudge.com.br/judge/pt/problems/view/1008
+/* SoluÃ§Ã£o exercicio https://www.urionlinejudge.com.br/judge/pt/problems/view/1008
 Andrey.*/
 
 #include <iostream>
