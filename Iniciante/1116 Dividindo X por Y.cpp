72903576fd9de349b110ab006f1eae5240c25464
AndreygFranca <andrey_franca@hotmail.com.br>
2015-12-30 20:12:43 -0200

    1116 Dividindo X por Y

---
+++ Iniciante/1116 Dividindo X por Y.cpp
@@ -0,0 +1,28 @@
+
+#include "stdio.h"
+#include "math.h"
+
+int main()
+{
+    int entrada = 0;
+    scanf("%d", &entrada);
+
+    for(int i = 0; i < entrada; i++)
+    {
+        int x = 0, y =0;
+        scanf("%d %d", &x, &y);
+        double resultado = 0;
+
+        if(y == 0)
+        {
+            printf("divisao impossivel\n");
+        }
+        else
+        {
+            resultado = (double)(x) / (double)(y);
+            printf("%.1lf\n", resultado);
+        }
+    }
+
+    return 0;
+}
